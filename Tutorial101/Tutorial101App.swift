9de2c880e29b087d9714ea701a32558fc31e1efe
ribbinpo <ribbinpo@gmail.com>
2023-09-20 16:00:04 +0700

    Initial Commit

---
+++ Tutorial101/Tutorial101App.swift
@@ -0,0 +1,17 @@
+//
+//  Tutorial101App.swift
+//  Tutorial101
+//
+//  Created by ธีระวุฒิ แซ่ซิ้ม on 20/9/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct Tutorial101App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
